4466032381b4a91fa8a1857cd7a3b67dac578da7
Lliurex <Lliurex@172.16.205.116>
2015-12-09 12:23:32 +0100

    script sql editado tareas add commit push automatizadas en build. nuevo target "distProduccion"

--- src/com/fpmislata/banco/persistence/migration/scripts/V001__Initial.sql
+++ src/com/fpmislata/banco/persistence/migration/scripts/V001__Initial.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE banco;
-
 CREATE TABLE IF NOT EXISTS `entidadbancaria` (
 	`idEntidadBancaria` INT(11) NOT NULL AUTO_INCREMENT,
 	`nombre` VARCHAR(50) NULL DEFAULT NULL,
@@ -13,53 +11,3 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=27
 ;
-
-
-INSERT INTO `entidadbancaria` (`idEntidadBancaria`, `nombre`, `codigoEntidad`, `fechaCreacion`, `direccion`, `cif`) VALUES
-	(3, 'CRISTIAN-BANK-PUTING', '9999', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(9, 'tucaixo', '0', NULL, NULL, NULL),
-	(10, 'samsa', '19', '2015-01-01', NULL, '23456'),
-	(12, 'samsa', '9', '2015-01-01', NULL, '23456'),
-	(13, 'CRISTIAN-BANK-PUTING', '6969', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(14, 'CRISTIAN-BANK-PUTING', '5555', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(15, 'CRISTIAN-BANK-PUTING', '4545', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(16, 'CRISTIAN-BANK-PUTING', '6666', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(17, 'CRISTIAN-BANK-PUTING', '8526', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(18, 'CRISTIAN-BANK-PUTING', '8888', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(19, 'CRISTIAN-BANK-PUTING', '1474', '2013-09-25', 'EN SU CRISTIANKELLYCOLEGA', '00000000'),
-	(20, 'a', '1111', NULL, NULL, NULL),
-	(21, 'juanito', '123', '2011-01-01', 'a', 'a12'),
-	(22, 'pepete', '0000', '2011-01-01', 'fafew', '23ef'),
-	(23, 'ab', '40', NULL, NULL, NULL),
-	(25, 'aaa', '10', NULL, NULL, NULL),
-	(26, 'fjaiefj', '010', NULL, NULL, NULL);
-
-
-/*
-CREATE TABLE `sucursalbancaria` (
-	`idSucursalBancaria` INT(11) NOT NULL AUTO_INCREMENT,
-	`nombre` VARCHAR(50) NOT NULL DEFAULT '0',
-	`numeroSucursal` INT(11) NOT NULL DEFAULT '0',
-	`direccion` VARCHAR(50) NOT NULL DEFAULT '0',
-	`telefono` INT(11) NOT NULL DEFAULT '0',
-	`director` VARCHAR(50) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`idSucursalBancaria`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
-
-
-
-CREATE TABLE `usuario` (
-	`idUsuario` INT(11) NOT NULL AUTO_INCREMENT,
-	`nombre` VARCHAR(50) NULL DEFAULT NULL,
-	`encryptedPassword` VARCHAR(50) NULL DEFAULT NULL,
-	`rol` VARCHAR(20) NULL DEFAULT NULL,
-	`nick` VARCHAR(50) NULL DEFAULT NULL,
-	PRIMARY KEY (`idUsuario`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
-*/
